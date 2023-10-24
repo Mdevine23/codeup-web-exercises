4023454975a37766e9f0da1ca916e1215559319b
Mark <mark.devine08@gmail.com>
2023-10-24 10:33:39 -0500

    CSS elements layout

--- JS Warm-ups/JS-warm-ups/guessNumber.js
+++ JS Warm-ups/JS-warm-ups/guessNumber.js
@@ -1,26 +1,39 @@
-const targetNumber = Math.floor(Math.random() * 100) + 1;
-console.log(`${targetNumber}`);
-let attempts = 0;
-function checkGuess() {
-    const userGuess = parseInt(prompt("Guess a number between 1 and 100:"));
+// const targetNumber = Math.floor(Math.random() * 100) + 1;
+// console.log(`${targetNumber}`);
+// let attempts = 0;
+// function checkGuess() {
+//     const userGuess = parseInt(prompt("Guess a number between 1 and 100:"));
+//
+//     do {
+//         console.log(attempts);
+//         if (userGuess > targetNumber) {
+//             parseInt(prompt("Guess a number lower."));
+//             attempts++;
+//             checkGuess();
+//         } else if (userGuess < targetNumber) {
+//             parseInt(prompt("Guess a number higher."));
+//             attempts++;
+//             checkGuess();
+//         }else {
+//             alert(`You got it!`)
+//         }
+//     } while(userGuess !== targetNumber)
+//
+// }
+// // its working but runs through the program twice during the loop. Needs extra work.
+//
+// checkGuess();
 
-    do {
-        console.log(attempts);
-        if (userGuess > targetNumber) {
-            parseInt(prompt("Guess a number lower."));
-            attempts++;
-            checkGuess();
-        } else if (userGuess < targetNumber) {
-            parseInt(prompt("Guess a number higher."));
-            attempts++;
-            checkGuess();
-        }else {
-            alert(`You got it!`)
+const findLongestWord = (values) => {
+    const toArray = values.split(" ");
+    let result = "";
+    for (let word of toArray) {
+        if(word.length > result.length) {
+            result = word;
         }
-    } while(userGuess !== targetNumber)
+    }
+    return result;
+};
 
-}
-// its working but runs through the program twice during the loop. Needs extra work.
-
-checkGuess();
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
 
