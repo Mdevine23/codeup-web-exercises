c24f3be1470d6fd8c3dff187b3f426479a24d7b1
Mark <mark.devine08@gmail.com>
2023-10-25 16:29:22 -0500

    media queries

--- JS Warm-ups/JS-warm-ups/fizz-buzz.js
+++ JS Warm-ups/JS-warm-ups/fizz-buzz.js
@@ -1,23 +1,65 @@
-const fizzBuzz = (start, end) => {
-    start = parseFloat(start);
-    end = parseFloat(end);
-    if (isNaN(start) || isNaN(end)) {
-        return false;
-    }
+// const fizzBuzz = (start, end) => {
+//     start = parseFloat(start);
+//     end = parseFloat(end);
+//     if (isNaN(start) || isNaN(end)) {
+//         return false;
+//     }
     // ^^^^ validate early so that the parameters WILL work ^^^^
 
-    for (let i = start; i <= end; i++) {
-        if (i % 15 === 0) {
-            console.log('FizzBuzz');
-        } else if (i % 5 === 0) {
-            console.log('Buzz');
-        } else if (i % 3 === 0) {
-            console.log('Fizz');
-        } else {
-            console.log(i)
-        }
-    }
-}
-fizzBuzz(0, 100);
+//     for (let i = start; i <= end; i++) {
+//         if (i % 15 === 0) {
+//             console.log('FizzBuzz');
+//         } else if (i % 5 === 0) {
+//             console.log('Buzz');
+//         } else if (i % 3 === 0) {
+//             console.log('Fizz');
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+// fizzBuzz(0, 100);
 
 // ^^^ using the tradition for loop, put your less likely scenario go in your first if conditional^^^
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+         [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+const createPairs = (array) => {
+    array = shuffleArray(array);
+    let pairs = [];
+    while (array.length > 0) {
+        let student1 = array.pop();
+        pairs.push(student1);
+        let student2 = array.pop();
+        pairs.push(student2);
+    }
+    return pairs;
+}
+
+
+const students = [
+    "Amanda",
+    "Bianca",
+    "Cameron",
+    "Chris",
+    "Dany,",
+    "David",
+    "Diana",
+    "Drew",
+    "Evan",
+    "Hassan",
+    "Jack",
+    "Jason",
+    "John",
+    "Johnathan",
+    "Jordan",
+    "Joshua",
+    "Katie",
+    "Lewis",
+];
+
+console.log(createPairs(students));
