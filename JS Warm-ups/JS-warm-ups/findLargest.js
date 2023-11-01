c7d2a8af9eb86936467c4aca77c2ae08b1687836
Mark <mark.devine08@gmail.com>
2023-11-01 11:52:01 -0500

    bootstrap exercises

--- JS Warm-ups/JS-warm-ups/findLargest.js
+++ JS Warm-ups/JS-warm-ups/findLargest.js
@@ -1,20 +1,38 @@
 // Function to find the largest number in an array
-const findLargestNumber = (arr) => {
-    let result = (Math.max(...arr));
-    // validate and return early
-    // start a variable "largest" at 0
-    // loop through arr and replace "largest" if value is greater
-    // return largest
-    return result;
-};
+// const findLargestNumber = (arr) => {
+//     let result = (Math.max(...arr));
+//     // validate and return early
+//     // start a variable "largest" at 0
+//     // loop through arr and replace "largest" if value is greater
+//     // return largest
+//     return result;
+// };
+//
+// (() => {
+//     // Sample array of numbers
+//     const numbers = [42, 17, 8, 94, 23, 61, 12, 51, 6];
+//     // Call the function and display the result
+//     const result = findLargestNumber(numbers);
+//     console.log(result);
+// })();
 
-(() => {
-    // Sample array of numbers
-    const numbers = [42, 17, 8, 94, 23, 61, 12, 51, 6];
-    // Call the function and display the result
-    const result = findLargestNumber(numbers);
-    console.log(result);
-})();
+// function digitalRoot(n) {
+//     if (isNaN(n)) {
+//         parseFloat(n);
+//     } else {
+//         return (n % 9);
+//     }
+// }
+// console.log(digitalRoot("12"));
 
-
-
+const isPangram = (str) => {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    for (let letter of alphabet) {
+        if (!str.toLowerCase().includes(letter)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+console.log(isPangram("The quick brown fox jumps over the lazy dog"));
